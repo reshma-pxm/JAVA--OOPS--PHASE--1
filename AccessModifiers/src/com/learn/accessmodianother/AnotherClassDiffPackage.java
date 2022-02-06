85f5af0d400eae7c15775521051e6cebdd5290ae
reshma-k <88098064+reshma-pxm@users.noreply.github.com>
2022-02-06 09:31:15 +0530

    Add files via upload

---
+++ AccessModifiers/src/com/learn/accessmodianother/AnotherClassDiffPackage.java
@@ -0,0 +1,13 @@
+package com.learn.accessmodianother;
+import com.learn.access.*;
+
+public class AnotherClassDiffPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AccessModifiers obj=new AccessModifiers();
+obj.publicmethod();
+
+	}
+
+}
