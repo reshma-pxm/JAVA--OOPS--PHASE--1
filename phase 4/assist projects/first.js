ac31ed7c2dd8af2fd4d8e82b9bad21e5db7e51ee
Kudaravalli Reshma <88098064+reshma-pxm@users.noreply.github.com>
2022-03-25 17:50:44 +0530

    assist projects

---
+++ phase 4/assist projects/first.js
@@ -0,0 +1,6 @@
+var a;
+a = 56;
+var b = 12;
+var fname = "Alex";
+console.log(a + b);
+console.log(fname);
---
+++ phase 4/assist projects/first.ts
@@ -0,0 +1,12 @@
+let a:number;
+a=56;
+
+let b:number=12;
+
+let fname:string="Alex";
+
+console.log(a+b);
+console.log(fname);
+
+
+
